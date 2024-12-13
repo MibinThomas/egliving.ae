ca5055811e069cd9b1cbc92a044432f569b00a66
Athul Chandran <m.athulchandran@gmail.com>
2024-12-13 15:16:03 +0530

    update code in contant page

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import PageHeader from "../../components/Page-header";
-import ContactInfo from "../../components/Contact-info";
-import ContactWithMap from "../../components/Contact-with-map";
+import dynamic from "next/dynamic";
+
+const PageHeader = dynamic(() => import("../../components/Page-header"), {
+  ssr: false,
+});
+const ContactInfo = dynamic(() => import("../../components/Contact-info"), {
+  ssr: false,
+});
+const ContactWithMap = dynamic(
+  () => import("../../components/Contact-with-map"),
+  { ssr: false }
+);
 import MainLayout from "../../layouts/main";
 
 const Contact = () => {
   React.useEffect(() => {
-    document.querySelector("body").classList.add("index3");
+    if (typeof window !== "undefined") {
+      document.querySelector("body").classList.add("index3");
+    }
   }, []);
+
   return (
     <MainLayout>
       <PageHeader
